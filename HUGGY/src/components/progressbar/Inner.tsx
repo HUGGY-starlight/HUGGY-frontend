37d554e9a87f7ef67b9d729c9f2a1893f7f76a6f
Jun99uu <igun0423@naver.com>
2023-02-24 13:40:48 +0900

    feat : add progress-bar component

---
+++ HUGGY/src/components/progressbar/Inner.tsx
@@ -0,0 +1,46 @@
+import { Props } from "./Wrapper";
+import styled from "styled-components/native";
+import { Animated } from "react-native";
+import { useRef, useEffect } from "react";
+
+const Inner = ({ stage, max }: Props) => {
+  const animation = useRef(new Animated.Value(0)).current;
+
+  const load = () => {
+    Animated.timing(animation, {
+      toValue: (stage / max) * 100, // 어떤 값으로 변경할지 - 필수
+      duration: 500, // 애니메이션에 걸리는 시간(밀리세컨드) - 기본값 500
+      delay: 0, // 딜레이 이후 애니메이션 시작 - 기본값 0
+      useNativeDriver: false, // 네이티브 드라이버 사용 여부 - 필수
+      isInteraction: true, // 사용자 인터랙션에 의해 시작한 애니메이션인지 지정 - 기본값 true
+      //easing: Easing.inOut(Easing.ease), // 애니메이션 속서 변경 함수 - 기본값 Easing.inOut(Easing.ease)
+    }).start(() => {
+      // 애니메이션 처리 완료 후 실행할 작업
+      // 햅틱 한 번 넣어야함
+    });
+  };
+
+  const width = animation.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+    extrapolate: "clamp",
+  });
+
+  useEffect(() => {
+    console.log(stage, max);
+    load();
+  }, [stage]);
+
+  return <InnerBox style={{ width: width }}></InnerBox>;
+};
+
+const InnerBox = styled(Animated.View)`
+  height: 100%;
+  background-color: #f5835e;
+  border-radius: 10px;
+  transition: all 0.15s;
+`;
+
+const AnimationBox = Animated.createAnimatedComponent(InnerBox);
+
+export default Inner;
---
+++ HUGGY/src/components/progressbar/Wrapper.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components/native";
+import { screenWidth } from "@style/dimensions";
+import Inner from "./Inner";
+
+export interface Props {
+  stage: number;
+  max: number;
+}
+
+const Wrapper = (props: Props) => {
+  return (
+    <ProgressContainer>
+      <ProgressBox>
+        <Inner {...props} />
+      </ProgressBox>
+    </ProgressContainer>
+  );
+};
+
+const ProgressContainer = styled.View`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  display: absolute;
+  top: 50px;
+`;
+
+const ProgressBox = styled.View`
+  width: ${screenWidth - 50}px;
+  height: 10px;
+  border-radius: 10px;
+  background-color: #ededed;
+`;
+
+export default Wrapper;
